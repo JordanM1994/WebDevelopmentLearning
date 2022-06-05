b960b00743e41e446c9cc99428b10cacd5bb6db7
Jordan McLeod <jordandmcleod@outlook.com>
2022-06-05 17:27:29 +0100

    completed jquery learning

--- JQuery/index.js
+++ JQuery/index.js
@@ -1,7 +1,42 @@
 
-$("h1").addClass("big-title margin-50")
+//Adds classes
+$("h1").addClass("big-title margin-50");
 
+//Adds eventlistener and calls a call back function
+$("h1").click(function() {
+  $("h1").css("color", "purple")
+});
 
-$("h1").text("What?!");
-
+//changes the html for buttons
 $("button").html("<em>Don't click!</em>");
+
+//console logs the images src attribute
+console.log($("img").attr("src"));
+
+// changesthe attribute that is specified, here it is a href
+$("a").attr("href", "https://www.yahoo.com");
+
+//adds an event listener for a click on a button
+// $("button").click(function() {
+//   $("h1").toggle();
+// });
+
+//adds an event listener for a click on a button to fade out
+// $("button").click(function() {
+//   $("h1").fadeToggle();
+// });
+
+//adds an event listener for a click on a button to fade out
+$("button").click(function() {
+  $("h1").animate({margin: 20});
+});
+
+//adds an event listener to the whole page that changes the h1
+$(document).keypress(function(event){
+  $("h1").text(event.key);
+});
+
+//adds an event listener for when the mouse hovers over the h1
+$("h1").on("mouseover", function() {
+  $("h1").css("color", "purple");
+})

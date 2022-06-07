361a92534e48cd3e84f5ba20dac6611be8666352
Jordan McLeod <jordandmcleod@outlook.com>
2022-06-07 17:21:19 +0100

    Created new folders, Front end and Back end, also started section NodeJS

---
+++ Back End/NodeJS/intro-to-node/index.js
@@ -0,0 +1,9 @@
+
+
+var superheroes = require("superheroes");
+var mySuperheroName = superheroes.random();
+console.log(mySuperheroName;
+
+const supervillains = require('supervillains');
+var mySuperVillainName = supervillains.random();
+console.log(mySuperVillainName;

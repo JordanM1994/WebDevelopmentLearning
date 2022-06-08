85803a01346e5717a501055ca316bd27df029c64
Jordan McLeod <jordandmcleod@outlook.com>
2022-06-08 08:50:10 +0100

    beautified code

--- Back End/my-express-server/server.js
+++ Back End/my-express-server/server.js
@@ -1,12 +1,11 @@
-
 const express = require('express');
 
 const app = express()
 
-app.get("/", function(req, res){
+app.get("/", function(req, res) {
   response.send("Hello");
 })
 
-app.listen(3000, function(){
+app.listen(3000, function() {
   console.log("Server started on port 3000")
 })

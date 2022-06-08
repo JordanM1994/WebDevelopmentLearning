d568a4ba780d2a977c48f2f29fad17a7b6ff1741
Jordan McLeod <jordandmcleod@outlook.com>
2022-06-08 08:49:39 +0100

    Created first express server with req and res

---
+++ Back End/my-express-server/server.js
@@ -0,0 +1,12 @@
+
+const express = require('express');
+
+const app = express()
+
+app.get("/", function(req, res){
+  response.send("Hello");
+})
+
+app.listen(3000, function(){
+  console.log("Server started on port 3000")
+})

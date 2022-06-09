6c066feea3da2a3124accc6b36b28b2bd72287ad
Jordan McLeod <jordandmcleod@outlook.com>
2022-06-09 06:54:16 +0100

    Created calculator and started BMI calc

--- Back End/my-express-server/server.js
+++ Back End/my-express-server/server.js
@@ -1,11 +1,23 @@
 const express = require('express');
 
-const app = express()
+const app = express();
 
 app.get("/", function(req, res) {
-  response.send("Hello");
-})
+  res.send("<h1>Hello</h1>");
+});
+
+app.get("/contact", function(req, res) {
+  res.send("Contact me at: jordandmcleod@outlook.com");
+});
+
+app.get("/about", function(req, res) {
+  res.send("My name is Jordan and I am a full stack developer :D ");
+});
+
+app.get("/hobbies", function(req, res) {
+  res.send("Coffee, Code and Beer :D ");
+});
 
 app.listen(3000, function() {
   console.log("Server started on port 3000")
-})
+});
